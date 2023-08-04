f9e7b25997d88e0d53ea494f643947bca34a5eb8
Rasik Abdullah <rasikabdullah@gmail.com>
2023-08-05 02:27:37 +0600

    chatsPage added

--- frontend/src/components/AuthPage/AuthPage.jsx
+++ frontend/src/components/AuthPage/AuthPage.jsx
@@ -1,30 +1,38 @@
 // import { MdWavingHand } from "react-icons/md";
+import axios from 'axios'
 
 const AuthPage = (props) => {
 
     const onSubmit = (e) => {
-      e.preventDefault();
-      const { value } = e.target[0];
-      props.onAuth({ username: value, secret: value });
+        e.preventDefault();
+        const { value } = e.target[0];
+
+        axios.post(
+            'http://localhost:5000/authenticate',
+            { username: value }
+        )
+        .then(r => props.onAuth({...r.data, secret: value}))
+        .catch(e => console.log('error', e))
+        props.onAuth({ username: value, secret: value });
     };
-  
+
     return (
-      <div className="background">
-        <form onSubmit={onSubmit} className="form-card">
-          <div className="form-title">Welcome 👋</div>
-  
-          <div className="form-subtitle">Set a username to get started</div>
-  
-          <div className="auth">
-            <div className="auth-label">Username</div>
-            <input className="auth-input" name="username" />
-            <button className="auth-button" type="submit">
-              Enter
-            </button>
-          </div>
-        </form>
-      </div>
+        <div className="background">
+            <form onSubmit={onSubmit} className="form-card">
+                <div className="form-title">Welcome 👋</div>
+
+                <div className="form-subtitle">Set a username to get started</div>
+
+                <div className="auth">
+                    <div className="auth-label">Username</div>
+                    <input className="auth-input" name="username" />
+                    <button className="auth-button" type="submit">
+                        Enter
+                    </button>
+                </div>
+            </form>
+        </div>
     );
-  };
-  
-  export default AuthPage;
+};
+
+export default AuthPage;
--- frontend/src/components/ChatsPage/ChatsPage.jsx
+++ frontend/src/components/ChatsPage/ChatsPage.jsx
@@ -1,8 +1,23 @@
+import {
+    MultiChatSocket,
+    MultiChatWindow,
+    useMultiChatLogic,
+} from 'react-chat-engine-advanced';
 
-const ChatsPage = () => {
+// const CHAT_ENGINE_PROJECT_ID = import.meta.env.CHAT_ENGINE_PROJECT_ID;
+
+const ChatsPage = (props) => {
+
+    const chatProps = useMultiChatLogic(
+        'fc7521a7-01a9-4d7d-8386-f06125a46b0e',
+        props.user.username,
+        props.user.secret
+    );
+
     return (
-        <div>
-            chats
+        <div style={{ height: "100vh", width: "100vw" }}>
+            <MultiChatSocket {...chatProps} />
+            <MultiChatWindow {...chatProps} style={{ height: '100%' }} />
         </div>
     );
 };
